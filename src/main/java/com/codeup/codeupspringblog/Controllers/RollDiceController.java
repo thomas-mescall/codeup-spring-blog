4ad830e6dd5fbbd952ac38a07e4c1933f88f4a3b
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-05-17 16:58:59 -0500

    working on views

---
+++ src/main/java/com/codeup/codeupspringblog/Controllers/RollDiceController.java
@@ -0,0 +1,19 @@
+package com.codeup.codeupspringblog.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice/n")
+    @ResponseBody
+    public int diceRoll() {
+        Random random = new Random();
+        return random.nextInt(6);
+    }
+
+
+}
