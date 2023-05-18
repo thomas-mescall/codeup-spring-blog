5bf4579f73fa85fd3f6b7073b854ae7af2976dc4
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-05-18 16:59:13 -0500

    working on Q4

--- src/main/java/com/codeup/codeupspringblog/models/Post.java
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -1,27 +1,63 @@
 package com.codeup.codeupspringblog.models;
 
-public class Post {
-    private String title;
-    private String body;
+import jakarta.persistence.*;
 
-    public Post(String title, String body) {
-        this.title = title;
-        this.body = body;
+@Entity
+@Table(name = "posts")
+public class Post {
+//    private String title;
+//    private String body;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
+//    public Post(String title, String body) {
+//        this.title = title;
+//        this.body = body;
+//    }
+
+    public Post() {
+
     }
+
+    @Column(nullable = false, length = 50)
+        private String title;
+
+    @Column(nullable = false)
+    private String description;
+
 
     public String getTitle() {
         return title;
     }
 
-    public String getBody() {
-        return body;
+//    public String getBody() {
+//        return body;
+//    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setBody(String body) {
-        this.body = body;
+//    public void setBody(String body) {
+//        this.body = body;
+//    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
