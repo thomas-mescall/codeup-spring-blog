9c22c16c0b78545cb72e4a4fcbab5f9ceeaafc3c
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-05-18 16:38:31 -0500

    catching up

--- src/main/java/com/codeup/codeupspringblog/Controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/Controllers/PostController.java
@@ -1,31 +1,42 @@
 package com.codeup.codeupspringblog.Controllers;
 
+import com.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String post() {
-        return "posts index page";
+    public String showAllPosts(Model model) {
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post("Title 1", "Body 1"));
+        posts.add(new Post("Title 2", "Body 2"));
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String id(@PathVariable String id) {
-        return id;
+    public String showPost(@PathVariable String id, Model model) {
+        String title = "This is a title";
+        String body = "This is a body";
+        Post post = new Post(title, body);
+        model.addAttribute("post", post);
+        return "posts/show";
     }
     @GetMapping("posts/create")
     @ResponseBody
     public String view() {
-        return "view the form for creating a post";
+        return "posts/create";
     }
     @PostMapping("posts/create")
     @ResponseBody
     public String create() {
-        return "create a post";
+        return "redirects:/posts";
     }
 }
---
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -0,0 +1,27 @@
+package com.codeup.codeupspringblog.models;
+
+public class Post {
+    private String title;
+    private String body;
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
