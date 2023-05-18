e9920d34ae46c8ed35dce8b6cd1d4d5ef746c7e5
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-05-18 15:37:55 -0500

    need to come back to fix

--- src/main/java/com/codeup/codeupspringblog/Controllers/RollDiceController.java
+++ src/main/java/com/codeup/codeupspringblog/Controllers/RollDiceController.java
@@ -1,7 +1,9 @@
 package com.codeup.codeupspringblog.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
@@ -12,10 +14,26 @@ public class RollDiceController {
     public String intro() {
         return "roll-dice";
     }
-//    public int diceRoll() {
-//        Random random = new Random();
-//        return random.nextInt(6);
-//    }
+
+    public String getNum(@PathVariable int chosen, Model model) {
+        int roll = diceRoll();
+        model.addAttribute("roll", roll);
+        model.addAttribute("chosen", chosen);
+        model.addAttribute("message", getMessage(roll, chosen));
+        return "result";
+    }
+    public int diceRoll() {
+        Random random = new Random();
+        return random.nextInt(6);
+    }
+
+    private String getMessage(int roll, int chosen) {
+        if (roll == chosen) {
+            return "Congratulations! You guessed correctly!";
+        } else {
+            return "Sorry! You guessed wrong. Try again!";
+        }
+    }
 
 
 }
